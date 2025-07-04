72fc386fc24ca540eeace261764e4cb3051921c2
Muhammad Waqas Rashid <102940015+WaqasAly@users.noreply.github.com>
2025-07-05 01:09:50 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Header from "./components/Header";
+import TaskList from "./components/TaskList";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Header/>} />
+        <Route path="/" element={<Dashboard />} />
       </Routes>
     </Router>
   );
